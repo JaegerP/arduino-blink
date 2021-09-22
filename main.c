e0b3ebbdf6b195bd4c9ba65b0c37d2cf83256298
JaegerP <jaegerp@myumanitoba.ca>
2021-09-22 16:04:17 +0200

    use timer interrupts instead if delay function

--- main.c
+++ main.c
@@ -14,14 +14,44 @@
 
 
 #include<avr/io.h>
-#include<util/delay.h>
+#include<avr/interrupt.h>
 
+volatile uint8_t count = 0;
+
+void set_timer_p1024_ms(uint16_t __t) {
+	// enable compare and overflow interrupt
+	TIMSK1 |= ((1 << OCIE1A) | (1 << TOIE1));
+
+	// clear config registers
+	TCCR1A = 0x00;
+	TCCR1A = 0x00;
+
+	// set CTC mode
+	//TCCR1A |= ((1 << COM1A1) | (1 << COM1A0));
+	TCCR1B |= 1 << WGM12;
+
+	// 1024 prescaler
+	TCCR1B |= ((1 << CS12) | (1 << CS10));
+
+	// compare to this value
+	OCR1A = __t * 16;
+
+	sei();
+}
 
 int main (void) {
 	DDRB = 1 << DDB5;
+	set_timer_p1024_ms(2000);
 	while (1) {
-		PORTB ^= 1<<PB5; 
-		_delay_ms(1000);
+
+		if (count >= 1) {
+			PORTB ^= 1<<PB5;
+			count = 0;
+		}
 	}
 	return 0;
+}
+
+ISR(TIMER1_COMPA_vect) {
+	++count;
 }
