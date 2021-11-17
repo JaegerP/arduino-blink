68cecd4b578d665d791d94a2fb874e4d0f4a3934
Philipp Jaeger <p@pj4e.de>
2021-11-17 16:14:59 +0100

    Fix index resolving in eclipse

--- main.c
+++ main.c
@@ -11,7 +11,9 @@
  * ----------------------------------------------------------------------------
  */
 
-
+#ifndef __AVR_ATmega328P__
+#define __AVR_ATmega328P__
+#endif
 
 #include<avr/io.h>
 #include<avr/interrupt.h>
